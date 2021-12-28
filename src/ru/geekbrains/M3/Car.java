9f501bf7fc3ed4f6f6feb8f205046fb6391fd5e1
isaka <isakasi12242001@gmail.com>
2021-12-28 21:40:49 +0300

    delete implements method run from runnable interface

--- src/ru/geekbrains/M3/Car.java
+++ src/ru/geekbrains/M3/Car.java
@@ -1,10 +1,14 @@
 package ru.geekbrains.M3;
 
-public class Car implements Runnable {
+
+import java.util.concurrent.*;
+
+public class Car   {
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
@@ -19,20 +23,10 @@ public class Car implements Runnable {
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+
     }
-    @Override
-    public void run() {
-        try {
-            System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
-            System.out.println(this.name + " готов");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
-        }
-    }
+        CountDownLatch  cdl = new CountDownLatch(4);
+
 }
 
 
